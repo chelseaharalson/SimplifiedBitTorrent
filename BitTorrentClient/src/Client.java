d05fb851c886e65f878ed82c53d5a2b93001b1b8
Chelsea Metcalf <chelseametcalf@ufl.edu>
2016-05-29 02:21:02 -0400

    Able to transmit all files.

--- BitTorrentClient/src/Client.java
+++ BitTorrentClient/src/Client.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.*;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,10 +9,11 @@ import java.util.List;
  * @author chelsea metcalf
  */
 public class Client {
+    static String textFileList = "fileNameListDOWNLOADED.txt";
     static String FILE_TO_RECEIVE = "flowerimageDOWNLOADED.jpg";
     static int FILE_SIZE = 6022386;
     
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         if (args.length != 2) {
             System.err.println("Usage: java Client <host name> <port number>");
             System.exit(1);
@@ -28,29 +30,78 @@ public class Client {
         try {
           sock = new Socket(hostName, portNumber);
           System.out.println("Connecting...");
+          
+          receive(sock);
+          
+          //List<File> fList = new ArrayList<File>();
+          
+          // receive files
+          /*for (int i = 0; i < 43; i++) {
+            byte[] byteArray  = new byte [FILE_SIZE];
+            InputStream is = sock.getInputStream();
+            fos = new FileOutputStream("name" + i);
+            bos = new BufferedOutputStream(fos);
+            bytesRead = is.read(byteArray,0,byteArray.length);
+            current = bytesRead;
 
-          // receive file
-          byte [] mybytearray  = new byte [FILE_SIZE];
-          InputStream is = sock.getInputStream();
-          fos = new FileOutputStream(FILE_TO_RECEIVE);
-          bos = new BufferedOutputStream(fos);
-          bytesRead = is.read(mybytearray,0,mybytearray.length);
-          current = bytesRead;
+            do {
+               bytesRead = is.read(byteArray, current, (byteArray.length-current));
+               if(bytesRead >= 0) current += bytesRead;
+            } while(bytesRead > -1);
 
-          do {
-             bytesRead = is.read(mybytearray, current, (mybytearray.length-current));
-             if(bytesRead >= 0) current += bytesRead;
-          } while(bytesRead > -1);
-
-          bos.write(mybytearray, 0 , current);
-          bos.flush();
-          System.out.println("File " + FILE_TO_RECEIVE
-              + " downloaded (" + current + " bytes read)");
+            bos.write(byteArray, 0 , current);
+            bos.flush();
+            System.out.println("File " + "name"+i + " downloaded (" + current + " bytes read)");
+          }*/
         }
         finally {
-          if (fos != null) fos.close();
-          if (bos != null) bos.close();
-          if (sock != null) sock.close();
+          //if (fos != null) fos.close();
+          //if (bos != null) bos.close();
+          //if (sock != null) sock.close();
+        }
+    }
+    
+    /*public static void saveFileChunk(int x, byte[] chunk) {
+        try {
+            FileOutputStream fso = new FileOutputStream("FileChunks/" + x, false);
+            fso.write(chunk);
+            fso.flush();
+            fso.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }*/
+    
+    public static void receive(Socket socket){
+        try {
+            DataInputStream dis = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+            //read the number of files from the server
+            int number = dis.readInt();
+            ArrayList<File>files = new ArrayList<File>(number);
+            System.out.println("Number of Files to be received: " +number);
+            //read file names and add files to arraylist
+            for(int i = 0; i< number;i++){
+                File file = new File(dis.readUTF());
+                files.add(file);
+            }
+            int n = 0;
+            byte[] buf = new byte[FILE_SIZE];
+
+            // receive files
+            for(int i = 0; i < files.size(); i++){
+                System.out.println("Receiving File: " + files.get(i).getName());
+                FileOutputStream fos = new FileOutputStream("FileChunks/" + files.get(i).getName());
+                while((n = dis.read(buf)) != -1 && n!=3 ) {
+                  fos.write(buf,0,n);
+                  fos.flush();
+                }
+                fos.close();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
         }
     }
     
--- BitTorrentServer/src/FileOwner.java
+++ BitTorrentServer/src/FileOwner.java
@@ -1,14 +1,17 @@
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.nio.file.Files;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -25,7 +28,8 @@ public class FileOwner {
     static FileInputStream fis = null;
     static BufferedInputStream bis = null;
     static OutputStream os = null;
-    static String FILE_TO_SEND = "FileChunks/flowerimage.jpg";
+    //static String FILE_TO_SEND = "FileChunks/flowerimage.jpg";
+    //static String textFile = "fileNameList.txt";
 
     public static void main(String[] args) throws IOException {
         if (args.length != 1) {
@@ -33,13 +37,14 @@ public class FileOwner {
             System.exit(1);
         }
         int portNumber = Integer.parseInt(args[0]);
-        /*try {
+        try {
             splitFile(new File("FileChunks/flowerimage.jpg"));
-            System.out.println(fileList);
-            mergeFiles(fileList, new File("FileChunks/merge.jpg"));
+            createTextFileList(fileList);
+            //System.out.println(fileList);
+            //mergeFiles(fileList, new File("FileChunks/merge.jpg"));
         } catch (IOException ex) {
             Logger.getLogger(FileOwner.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
+        }
         
         FileInputStream fis = null;
         BufferedInputStream bis = null;
@@ -53,17 +58,22 @@ public class FileOwner {
             try {
               sock = serverSocket.accept();
               System.out.println("Accepted connection: " + sock);
-              // send file
-              File myFile = new File (FILE_TO_SEND);
-              byte [] mybytearray  = new byte [(int)myFile.length()];
-              fis = new FileInputStream(myFile);
-              bis = new BufferedInputStream(fis);
-              bis.read(mybytearray,0,mybytearray.length);
-              os = sock.getOutputStream();
-              System.out.println("Sending " + FILE_TO_SEND + "(" + mybytearray.length + " bytes)");
-              os.write(mybytearray,0,mybytearray.length);
-              os.flush();
-              System.out.println("Done.");
+              
+              sendFiles(fileList, sock);
+              
+              // send files
+              /*for (int i = 0; i < fileList.size(); i++) {
+                  System.out.println(fileList.get(i).getName());
+                  byte[] byteArray = new byte [(int)fileList.get(i).length()];
+                  fis = new FileInputStream(fileList.get(i));
+                  bis = new BufferedInputStream(fis);
+                  bis.read(byteArray,0,byteArray.length);
+                  os = sock.getOutputStream();
+                  System.out.println("Sending " + fileList.get(i).getName() + " (" + byteArray.length + " bytes)");
+                  os.write(byteArray,0,byteArray.length);
+                  os.flush();
+                  System.out.println("Done.");
+              }*/
             }
             finally {
               if (bis != null) bis.close();
@@ -94,18 +104,66 @@ public class FileOwner {
                     if (!fileList.contains(newFile)) {
                         fileList.add(newFile);
                     }
-                    System.out.println("Total # of pieces file is divided into: " + partCounter);
+                    System.out.println("Total # of pieces file is divided into: " + (partCounter-1));
                 }
             }
         }
     }
     
-    public static void mergeFiles(List<File> files, File into) throws IOException {
+    public static void createTextFileList(List<File> fList) throws FileNotFoundException, IOException {
+        BufferedWriter writer = null;
+        File outputFile = new File("fileNameList.txt");
+        writer = new BufferedWriter(new FileWriter(outputFile));
+        String flist = "";
+        for (int i = 0; i < fList.size(); i++) {
+            flist = flist + fList.get(i) + "\n";
+        }
+        flist = flist.trim();
+        writer.write(flist);
+        writer.close();
+    }
+    
+    public static void sendFiles(List<File> files, Socket socket) {
+        try {
+            DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+            System.out.println(files.size());
+            //write the number of files to the server
+            dos.writeInt(files.size());
+            dos.flush();
+
+            //write file names 
+            for(int i = 0 ; i < files.size(); i++) {
+                dos.writeUTF(files.get(i).getName());
+                dos.flush();
+            }
+
+            int n = 0;
+            byte[]buf = new byte[4092];
+            byte[] done = new byte[3];
+            String str = "done";  // randomly anything
+            done = str.getBytes();
+            for(int i = 0; i < files.size(); i++){
+                System.out.println("Sending File: " + files.get(i).getName());
+                FileInputStream fis = new FileInputStream(files.get(i));
+                while((n = fis.read(buf)) != -1){
+                    dos.write(buf,0,n);
+                    dos.flush();
+                }              
+                dos.write(done,0,3);
+                dos.flush();
+            }
+            dos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }	
+    }
+    
+    /*public static void mergeFiles(List<File> files, File into) throws IOException {
         try (BufferedOutputStream mergingStream = new BufferedOutputStream(new FileOutputStream(into))) {
             for (File f : files) {
                 Files.copy(f.toPath(), mergingStream);
             }
         }
-    }
+    }*/
 
 }
